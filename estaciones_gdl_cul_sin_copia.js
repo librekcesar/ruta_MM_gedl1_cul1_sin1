1c41409c9da152795a3c17b4b302596a4ced1773
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-14 16:43:40 -0600

    Add files via upload

---
+++ estaciones_gdl_cul_sin_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_gdl_cul_sin",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "RG5X+M4V, Culiacán, Sinaloa", "description": "SIN1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -107.4522006, 24.8092373, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "6HMM+PWG, Mazatlán, Sinaloa", "description": "CUL1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -106.4151601, 23.2343537, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "20.624283851536905, -103.2893056", "description": "GDL1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -103.2892967, 20.6243079, 0.0 ] } }
+]
+}
---
+++ paradas_gdl_cul_sin_copia.js
@@ -0,0 +1,10 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_gdl_cul_sin",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Estación 13524 - Servicio Gasolinero de la Costa 2", "description": "Carretera Federal No. 15 Tepic - Mazatlán 25+160 No. 53, Entre crucero de San Blas y poblado del Jicote, 63510 Tepic, Nay.", "layer": "PEMES_GDL1_CUL_SIN1", "direccion": "Carretera Federal No. 15 Tepic - Mazatlán 25+160 No. 53, Entre crucero de San Blas y poblado del Jic" }, "geometry": { "type": "Point", "coordinates": [ -105.0531111, 21.6989238, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex", "description": "Carretera Maxipista, Mazatlan-Culiacán Km. 90, 240, 82700 Sin.", "layer": "PEMES_GDL1_CUL_SIN1", "direccion": "Carretera Maxipista, Mazatlan-Culiacán Km. 90, 240, 82700 Sin." }, "geometry": { "type": "Point", "coordinates": [ -106.9292882, 23.9197693, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Carretera Fed Mexico 40 Maz-Tepic Km 239.50, 82210 Sinaloa", "layer": "PEMES_GDL1_CUL_SIN1", "direccion": "Carretera Fed Mexico 40 Maz-Tepic Km 239.50, 82210 Sinaloa" }, "geometry": { "type": "Point", "coordinates": [ -106.1947297, 23.1967627, 0.0 ] } }
+]
+}
